6d812f065a731c925f0b04dedc457a6eb81812b0
Devesh Gokul <129370915+ST10375204@users.noreply.github.com>
2025-07-25 13:19:29 +0200

    Update Program.cs

--- tycoonApiTest/tycoonAPI/Program.cs
+++ tycoonApiTest/tycoonAPI/Program.cs
@@ -2,27 +2,30 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080); 
+});
+
+// Add services
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "tycoonAPI", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "tycoonAPI", Version = "v1" });
 });
-builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "tycoonAPI v1");
-    });
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "tycoonAPI v1");
+    c.RoutePrefix = ""; 
+});
+
+
 app.MapControllers();
 
 app.Run();
