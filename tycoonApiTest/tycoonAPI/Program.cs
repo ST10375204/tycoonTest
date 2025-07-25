7256c7c558a189c153ceeef7c5e41f5af7fddf95
Devesh Gokul <129370915+ST10375204@users.noreply.github.com>
2025-07-25 13:08:04 +0200

    Update Program.cs

--- tycoonApiTest/tycoonAPI/Program.cs
+++ tycoonApiTest/tycoonAPI/Program.cs
@@ -1,18 +1,28 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddControllers(); 
+// Add services to the container
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "tycoonAPI", Version = "v1" });
+});
+builder.Services.AddControllers();
 
 var app = builder.Build();
-app.MapControllers();
-// Configure the HTTP request pipeline.
+
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "tycoonAPI v1");
+    });
 }
 
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
-
